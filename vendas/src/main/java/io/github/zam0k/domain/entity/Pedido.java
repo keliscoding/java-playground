378fb62b293906d6ff97be52e38f885a91aa7567
Kelly Castelo <kawokkun@gmail.com>
2022-08-05 11:53:53 -0300

    criando as classes de modelo

---
+++ vendas/src/main/java/io/github/zam0k/domain/entity/Pedido.java
@@ -0,0 +1,45 @@
+package io.github.zam0k.domain.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class Pedido {
+
+	private Integer id;
+	private LocalDateTime dataPedido;
+	private BigDecimal total;
+	private Cliente cliente;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalDateTime getDataPedido() {
+		return dataPedido;
+	}
+
+	public void setDataPedido(LocalDateTime dataPedido) {
+		this.dataPedido = dataPedido;
+	}
+
+	public BigDecimal getTotal() {
+		return total;
+	}
+
+	public void setTotal(BigDecimal total) {
+		this.total = total;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+}
