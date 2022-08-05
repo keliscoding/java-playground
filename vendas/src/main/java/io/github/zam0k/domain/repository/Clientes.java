5ed2e6ed0bf21a1fd0bcad7ef73087c3e3379360
Kelly Castelo <kawokkun@gmail.com>
2022-08-05 12:41:31 -0300

    spring data

--- vendas/src/main/java/io/github/zam0k/domain/repository/Clientes.java
+++ vendas/src/main/java/io/github/zam0k/domain/repository/Clientes.java
@@ -2,62 +2,15 @@ package io.github.zam0k.domain.repository;
 
 import java.util.List;
 
-import javax.persistence.EntityManager;
-import javax.persistence.TypedQuery;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import io.github.zam0k.domain.entity.Cliente;
 
 //repository são classes que acessam a base de dados
-@Repository
-public class Clientes {
 
-	@Autowired
-	private EntityManager entityManager;
+public interface Clientes extends JpaRepository<Cliente, Integer> {
 
-	@Transactional
-	public Cliente salvar(Cliente cliente) {
-		entityManager.persist(cliente);
-		return cliente;
-	}
-
-	@Transactional
-	public Cliente atualizar(Cliente cliente) {
-		entityManager.merge(cliente);
-		return cliente;
-	}
-
-	@Transactional
-	public void deletar(Cliente cliente) {
-		if (!entityManager.contains(cliente)) {
-			cliente = entityManager.merge(cliente);
-		}
-		entityManager.remove(cliente);
-	}
-
-	@Transactional
-	public void deletar(Integer id) {
-		Cliente cliente = entityManager.find(Cliente.class, id);
-		entityManager.remove(cliente);
-	}
-
-	@Transactional(readOnly = true)
-	public List<Cliente> listarClientes() {
-		return entityManager
-				.createQuery("from Cliente", Cliente.class)
-				.getResultList();
-	}
-
-	@Transactional(readOnly = true)
-	public List<Cliente> buscarPorNome(String nome) {
-		String jpql = " select c from Cliente c where c.nome like :nome ";
-		TypedQuery<Cliente> query = entityManager.createQuery(jpql,
-				Cliente.class);
-		query.setParameter("nome", "%" + nome + "%");
-		return query.getResultList();
-	}
+	// Query Methods
+	public List<Cliente> findByNomeLike(String nome);
 
 }
