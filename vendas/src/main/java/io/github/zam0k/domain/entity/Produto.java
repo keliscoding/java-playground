5b4e4f13c563af1d6e192cfd3f5f44367bd177e4
Kelly Castelo <kawokkun@gmail.com>
2022-08-07 11:48:40 -0300

    Adicionar internacionalização e config

--- vendas/src/main/java/io/github/zam0k/domain/entity/Produto.java
+++ vendas/src/main/java/io/github/zam0k/domain/entity/Produto.java
@@ -30,11 +30,11 @@ public class Produto {
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 
-	@NotEmpty(message = "Campo descrição é obrigatório.")
+	@NotEmpty(message = "{campo.descricao.obrigatorio}")
 	private String descricao;
 
 	@Column(name = "preco_unitario", precision = 20, scale = 2)
-	@NotNull(message = "Campo preço é obrigatório.")
+	@NotNull(message = "{campo.preco.obrigatorio}")
 	private BigDecimal precoUnitario;
 
 }
