f28c0e2ded2fbf3164eb289a167a7a7aead6403a
Kelly Castelo <kawokkun@gmail.com>
2022-08-15 09:25:44 -0300

    implementando o microservice de clientes

---
+++ microsservicos/msclientes/src/main/java/io/github/zam0k/msclientes/application/ClienteService.java
@@ -0,0 +1,24 @@
+package io.github.zam0k.msclientes.application;
+
+import io.github.zam0k.msclientes.domain.Cliente;
+import io.github.zam0k.msclientes.domain.infra.repository.ClienteRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ClienteService {
+    private final ClienteRepository repository;
+
+    @Transactional
+    public Cliente save(Cliente cliente) {
+        return repository.save(cliente);
+    }
+
+    public Optional<Cliente> getByCPF(String cpf) {
+        return repository.findByCpf(cpf);
+    }
+}
