ce3da23adf12606821c44e12e76316fb822123bd
Kelly Castelo <kawokkun@gmail.com>
2022-08-17 10:29:13 -0300

    refactoring do controller de pessoas

--- rest-with-spring-boot-and-java-erudio/src/main/java/io/github/zam0k/restwithspringbootandjavaerudio/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/io/github/zam0k/restwithspringbootandjavaerudio/controllers/PersonController.java
@@ -4,6 +4,7 @@ import io.github.zam0k.restwithspringbootandjavaerudio.model.Person;
 import io.github.zam0k.restwithspringbootandjavaerudio.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,40 +16,37 @@ public class PersonController {
     @Autowired
     private PersonServices service;
 
-    @RequestMapping(value = "/{id}",
-            method = RequestMethod.GET,
+    @GetMapping(value = "/{id}",
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable(value = "id") Long id) {
         return service.findById(id);
     }
 
-    @RequestMapping(method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findAll() {
         return service.findAll();
     }
 
-    @RequestMapping(method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Person create(@RequestBody Person person) {
         return service.create(person);
     }
 
-    @RequestMapping(
+    @PutMapping(
             value = "/{id}",
-            method = RequestMethod.PUT,
-            consumes = MediaType.APPLICATION_JSON_VALUE,
+            consumes = MediaType.APPLICATION_JSON_VALUE, //isso aqui é necessário por causa do swagger
             produces = MediaType.APPLICATION_JSON_VALUE)
     public Person update(@PathVariable(value = "id") Long id, @RequestBody Person person) {
         return service.update(person, id);
     }
 
-    @RequestMapping(
-            value = "/{id}",
-            method = RequestMethod.DELETE)
-    public void delete(@PathVariable(value = "id") Long id) {
+    @DeleteMapping(
+            value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable(value = "id") Long id) {
+
         service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
