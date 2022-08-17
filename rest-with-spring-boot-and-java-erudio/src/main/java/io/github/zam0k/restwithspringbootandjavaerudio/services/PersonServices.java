b0dc233583adc3fa39e59e4a769c8fbd0883093b
Kelly Castelo <kawokkun@gmail.com>
2022-08-17 08:50:03 -0300

    Retornando listas de objetos em json

---
+++ rest-with-spring-boot-and-java-erudio/src/main/java/io/github/zam0k/restwithspringbootandjavaerudio/services/PersonServices.java
@@ -0,0 +1,50 @@
+package io.github.zam0k.restwithspringbootandjavaerudio.services;
+
+import io.github.zam0k.restwithspringbootandjavaerudio.model.Person;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+@Service
+public class PersonServices {
+
+    private final AtomicLong counter = new AtomicLong();
+    private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public Person findById(String id) {
+        logger.info("Finding one person...");
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Leandro");
+        person.setLastName("Costa");
+        person.setAddress("Uberlandia - Minas Gerais - Brasil");
+        person.setGender("Male");
+        return person;
+    }
+
+    public List<Person> findAll() {
+        logger.info("Finding all people...");
+        List<Person> persons = new ArrayList<>();
+        for(int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+
+        return persons;
+
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name " + i);
+        person.setLastName("Last name " + i);
+        person.setAddress("Some address in Brasil " + i);
+        person.setGender("Male");
+        return person;
+    }
+
+}
