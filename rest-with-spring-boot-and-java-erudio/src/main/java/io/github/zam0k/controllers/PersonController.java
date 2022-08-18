d2adf118841b94389038cb3673af552942261415
Kelly Castelo <kawokkun@gmail.com>
2022-08-18 08:41:50 -0300

    Custom json serialization

--- rest-with-spring-boot-and-java-erudio/src/main/java/io/github/zam0k/controllers/PersonController.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/io/github/zam0k/controllers/PersonController.java
@@ -1,24 +1,16 @@
 package io.github.zam0k.controllers;
 
-import java.util.List;
-
+import io.github.zam0k.data.vo.v1.PersonVO;
+import io.github.zam0k.services.PersonServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import io.github.zam0k.data.vo.v1.PersonVO;
-import io.github.zam0k.services.PersonServices;
+import java.util.List;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("api/v1/person")
 public class PersonController {
 	
 	@Autowired
