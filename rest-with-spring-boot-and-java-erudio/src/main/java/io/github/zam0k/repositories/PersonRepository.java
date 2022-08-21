8bf580db7d7fff6eb07e4247df9c4d2d1cf25f5e
Kelly Castelo <kawokkun@gmail.com>
2022-08-21 08:12:35 -0300

    finalizando implementação e testando no postman

--- rest-with-spring-boot-and-java-erudio/src/main/java/io/github/zam0k/repositories/PersonRepository.java
+++ rest-with-spring-boot-and-java-erudio/src/main/java/io/github/zam0k/repositories/PersonRepository.java
@@ -12,6 +12,6 @@ import io.github.zam0k.model.Person;
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
     @Modifying
-    @Query("UPDATE Person p p.enabled = false WHERE p.id =:id")
+    @Query("UPDATE Person p SET p.enabled = false WHERE p.id =:id")
     void disablePerson(@Param("id") Long id);
 }
