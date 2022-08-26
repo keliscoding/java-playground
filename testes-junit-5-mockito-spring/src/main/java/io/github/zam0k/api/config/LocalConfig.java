0380fbbcf6d3605dd0c8cea4d38101a42e10efc8
Kelly Castelo <kawokkun@gmail.com>
2022-08-26 09:07:34 -0300

    perfil de testes

--- testes-junit-5-mockito-spring/src/main/java/io/github/zam0k/api/config/LocalConfig.java
+++ testes-junit-5-mockito-spring/src/main/java/io/github/zam0k/api/config/LocalConfig.java
@@ -1,2 +1,26 @@
-package io.github.zam0k.api.config;public class LocalConfig {
+package io.github.zam0k.api.config;
+
+import io.github.zam0k.api.domain.User;
+import io.github.zam0k.api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.List;
+
+@Configuration
+@Profile("local")
+public class LocalConfig {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Bean
+    public void startDB() {
+        User u1 = new User(null, "Valdir", "valdir@email.com", "123");
+        User u2 = new User(null, "Luiz", "luiz@email.com", "123");
+
+        repository.saveAll(List.of(u1, u2));
+    }
 }
