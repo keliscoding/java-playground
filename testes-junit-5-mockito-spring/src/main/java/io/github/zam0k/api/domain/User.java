4e8548e4f511a73e30322420ce06277d581a70e2
Kelly Castelo <kawokkun@gmail.com>
2022-08-26 07:55:47 -0300

    classe de dominio

---
+++ testes-junit-5-mockito-spring/src/main/java/io/github/zam0k/api/domain/User.java
@@ -0,0 +1,23 @@
+package io.github.zam0k.api.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+}
