46952d1e1ef278ada010e37f85bec6141277beaa
Kelly Castelo <kawokkun@gmail.com>
2022-08-26 08:22:09 -0300

    classe user resource findById

--- testes-junit-5-mockito-spring/src/main/java/io/github/zam0k/api/resources/UserResource.java
+++ testes-junit-5-mockito-spring/src/main/java/io/github/zam0k/api/resources/UserResource.java
@@ -1,6 +1,8 @@
 package io.github.zam0k.api.resources;
 
 import io.github.zam0k.api.domain.User;
+import io.github.zam0k.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,8 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/user")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Integer id){
-        return ResponseEntity.ok(new User(id, "Fulano", "Fulano@email.com", "Ful123"));
+        User user = service.findById(id);
+        return ResponseEntity.ok(user);
     }
 }
