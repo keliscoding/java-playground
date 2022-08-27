89bec212e926855af788da7a6904ad375135bd03
Kelly Castelo <kawokkun@gmail.com>
2022-08-27 09:30:31 -0300

    teste para manipulador de excecoes metodo data integrity

--- testes-junit-5-mockito-spring/src/test/java/io/github/zam0k/api/resources/exceptions/ResourceExceptionHandlerTest.java
+++ testes-junit-5-mockito-spring/src/test/java/io/github/zam0k/api/resources/exceptions/ResourceExceptionHandlerTest.java
@@ -1,21 +1,24 @@
 package io.github.zam0k.api.resources.exceptions;
 
+import io.github.zam0k.api.services.exceptions.DataIntegrityViolationException;
 import io.github.zam0k.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockHttpServletRequest;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.MockitoAnnotations.openMocks;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 @SpringBootTest
 class ResourceExceptionHandlerTest {
 
     public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado.";
+    public static final String EMAIL_JA_CADASTRADO = "Email já cadastrado";
     @InjectMocks
     private ResourceExceptionHandler handler;
 
@@ -31,14 +34,33 @@ class ResourceExceptionHandlerTest {
 
         assertNotNull(response);
         assertNotNull(response.getBody());
-        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+        assertEquals(NOT_FOUND, response.getStatusCode());
         assertEquals(ResponseEntity.class, response.getClass());
         assertEquals(StandardError.class, response.getBody().getClass());
         assertEquals(OBJETO_NAO_ENCONTRADO, response.getBody().getError());
         assertEquals(404, response.getBody().getStatus());
+        assertNotEquals("/user/2", response.getBody().getPath());
+
+        // teste que falha e não falha alternadamente, má prática? v
+        //assertNotEquals(LocalDateTime.now(), response.getBody().getTimestamp());
+
+        //System.out.println(LocalDateTime.now() + "  " + response.getBody().getTimestamp());
     }
 
     @Test
     void dataIntegrityViolation() {
+        ResponseEntity<StandardError> response = handler
+                .dataIntegrityViolation(
+                        new DataIntegrityViolationException(EMAIL_JA_CADASTRADO),
+                        new MockHttpServletRequest()
+                );
+        
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(BAD_REQUEST, response.getStatusCode());
+        assertEquals(ResponseEntity.class, response.getClass());
+        assertEquals(StandardError.class, response.getBody().getClass());
+        assertEquals(EMAIL_JA_CADASTRADO, response.getBody().getError());
+        assertEquals(400, response.getBody().getStatus());
     }
 }
