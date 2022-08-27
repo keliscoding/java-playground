0828d320da968755c366dbc6934b1375bba88e4a
Kelly Castelo <kawokkun@gmail.com>
2022-08-27 08:04:04 -0300

    teste para classe user service metodo update com excecao

--- testes-junit-5-mockito-spring/src/test/java/io/github/zam0k/api/services/impl/UserServiceImplTest.java
+++ testes-junit-5-mockito-spring/src/test/java/io/github/zam0k/api/services/impl/UserServiceImplTest.java
@@ -113,7 +113,30 @@ class UserServiceImplTest {
     }
 
     @Test
-    void update() {
+    void whenUpdateThenReturnSuccess() {
+        when(repository.save(any())).thenReturn(user);
+
+        User response = service.update(userDTO);
+
+        assertNotNull(response);
+        assertEquals(User.class, response.getClass());
+        assertEquals(ID, response.getId());
+        assertEquals(NAME, response.getName());
+        assertEquals(EMAIL, response.getEmail());
+        assertEquals(PASSWORD, response.getPassword());
+    }
+
+    @Test
+    void whenUpdateThenReturnDataIntegrityViolationException() {
+        when(repository.findByEmail(anyString())).thenReturn(optionalUser);
+
+        try {
+            optionalUser.get().setId(2);
+            service.update(userDTO);
+        } catch (Exception ex) {
+            assertEquals(DataIntegrityViolationException.class, ex.getClass());
+            assertEquals("Email já cadastrado", ex.getMessage());
+        }
     }
 
     @Test
