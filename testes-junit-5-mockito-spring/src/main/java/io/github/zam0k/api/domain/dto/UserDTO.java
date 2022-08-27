026831b00d364a62a74025a1fcb4b444f320a58d
Kelly Castelo <kawokkun@gmail.com>
2022-08-27 09:38:44 -0300

    removendo bug

--- testes-junit-5-mockito-spring/src/main/java/io/github/zam0k/api/domain/dto/UserDTO.java
+++ testes-junit-5-mockito-spring/src/main/java/io/github/zam0k/api/domain/dto/UserDTO.java
@@ -3,12 +3,14 @@ package io.github.zam0k.api.domain.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import static com.fasterxml.jackson.annotation.JsonProperty.Access.WRITE_ONLY;
 
 @Setter @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class UserDTO {
 
     private Integer id;
