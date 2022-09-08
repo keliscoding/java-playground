acdffd12b370d00cb257804b6ed124360833af1c
Kelly Castelo <kawokkun@gmail.com>
2022-09-08 10:28:21 -0300

    criando microservico cambio service

---
+++ microsservicos-erudio/cambio-service/src/test/java/io/github/zam0k/cambioservice/CambioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zam0k.cambioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
