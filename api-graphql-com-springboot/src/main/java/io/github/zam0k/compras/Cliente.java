123618badf62dd75b987a89e487f54067b2c5068
Kelly Castelo <kawokkun@gmail.com>
2022-09-15 08:03:13 -0300

    query com lista de clientes

---
+++ api-graphql-com-springboot/src/main/java/io/github/zam0k/compras/Cliente.java
@@ -0,0 +1,13 @@
+package io.github.zam0k.compras;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente {
+    private String name;
+    private String email;
+}
